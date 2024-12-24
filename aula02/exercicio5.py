93a4216ba25db0b73ddf2dcc844e39c798efa34e
Luiz <lgds.demtrio@gmail.com>
2024-12-24 17:56:50 -0300

    Resolução do exercicio

---
+++ aula02/exercicio5.py
@@ -0,0 +1,4 @@
+"""- Escreva um programa que calcule o quadrado de um número fornecido pelo usuário."""
+
+num1 = int(input("Inserir número para calcular o quadrado: "))
+print(f"O quadrado do {num1} é: {int(num1 * num1)}")
